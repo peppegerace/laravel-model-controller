114af86583abaa12ade86680247b09a0066af700
peppegerace <peppegerace47@gmail.com>
2023-11-20 18:06:08 +0100

    [add] print info movie card in DOM

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+
+@section('content')
+
+<h1>Movies</h1>
+
+<div class="row">
+
+    @foreach ($movies as $movie)
+
+    <div class="card m-3" style="width: 18rem;">
+        <div class="card-body">
+            <h4 class="card-title">Titolo: {{ $movie->title }}</h4>
+            <h5>Titolo originale: {{ $movie->original_title }}</h5>
+            <p class="card-text">Paese d'origine: {{ $movie->nationality}}</p>
+            <p class="card-text">Data d'uscita: {{ $movie->date }}</p>
+            <p class="card-text">Voto: {{ $movie->vote }}</p>
+        </div>
+    </div>
+
+    @endforeach
+
+</div>
+
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+    <title>Laravel Model Controller</title>
+</head>
+<body>
+
+    <div class="container my-5">
+        @yield('content')
+    </div>
+</body>
+</html>
